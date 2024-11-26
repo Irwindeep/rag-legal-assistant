98f6e74641d9e480f4417ea59d5dbb125a7237ae
Irwindeep <b22ai022@iitj.ac.in>
2024-11-26 10:36:05 +0530

    handling en_core_web_trf loading

--- src/query_decompose/decompose.py
+++ src/query_decompose/decompose.py
@@ -1,7 +1,7 @@
 from .preprocess import PreprocessAttachment
 import spacy
 from sentence_transformers import SentenceTransformer, util
-import subprocess
+from spacy.cli import download
 
 LEGAL_TASK_LABELS = {
     "case discovery": ["case law", "precedent", "legal cases"],
@@ -16,8 +16,7 @@ class Decomposer:
         try:
             self.nlp = spacy.load("en_core_web_trf")
         except OSError:
-            exc = subprocess.run(["python", "-m", "spacy", "download", "en_core_web_trf"], capture_output=True, text=True)
-            print(exc.returncode)
+            download("en_core_web_trf")
             self.nlp = spacy.load("en_core_web_trf")
 
         self.embedding_model = SentenceTransformer("nlpaueb/legal-bert-base-uncased")
